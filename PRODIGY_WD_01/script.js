d567430e58a759c0b98124a781456e415fc6d890
SandhiyaDhurairaj05 <sandydhurai@gmail.com>
2025-10-03 00:15:50 +0530

    Add files via upload

---
+++ PRODIGY_WD_01/script.js
@@ -0,0 +1,17 @@
+// Change navbar color on scroll
+window.addEventListener("scroll", function () {
+  const nav = document.getElementById("navbar");
+  if (window.scrollY > 50) {
+    nav.classList.add("scrolled");
+  } else {
+    nav.classList.remove("scrolled");
+  }
+});
+
+// Toggle mobile menu
+const menuToggle = document.getElementById("menu-toggle");
+const navLinks = document.querySelector(".nav-links");
+
+menuToggle.addEventListener("click", () => {
+  navLinks.classList.toggle("active");
+});
